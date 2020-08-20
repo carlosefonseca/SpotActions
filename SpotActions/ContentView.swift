c72f265c96096b5e307122e5ab55a14e3fab0f06
Carlos Fonseca <carlosefonseca@gmail.com>
2020-08-20 11:02:06 +0100

    Initial Commit

---
+++ SpotActions/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SpotActions
+//
+//  Created by carlos.fonseca on 20/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpotActions/SpotActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotActionsApp.swift
+//  SpotActions
+//
+//  Created by carlos.fonseca on 20/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SpotActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
