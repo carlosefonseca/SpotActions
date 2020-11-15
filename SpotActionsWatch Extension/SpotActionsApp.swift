420730ed1a73efc25c4610bde8e6e66c20144889
Carlos Fonseca <carlosefonseca@gmail.com>
2020-11-15 19:31:45 +0000

    Watch target

---
+++ SpotActionsWatch Extension/SpotActionsApp.swift
@@ -0,0 +1,18 @@
+//
+// SpotActionsApp.swift
+//
+
+import SwiftUI
+
+@main
+struct SpotActionsApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
